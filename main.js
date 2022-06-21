831a62db65518411c7157c9977a4897fec1827cb
Icebreaker380 <92775396+Icebreaker380@users.noreply.github.com>
2022-06-21 19:30:10 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+song1_status = "";
+song2_status = "";
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 function preload()
 {
@@ -33,6 +37,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + ", leftWristY = " + leftWristY);
@@ -46,4 +53,22 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+
+        if(song2_status == false)
+        {
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing - Peter Pan Song";
+        }
+    }
 }
